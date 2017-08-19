8b160f8289ea6a9b43c1acc6a0b25f38bf781b9e
Karl Wagner <karl@springsup.com>
2017-08-19 17:02:05 +0200

    another go

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ webos3Accessory.prototype.setState = function(state, callback) {
       if (this.wakeWithCEC) {
 	      // Attempt to wake via CEC.
 	      this.log("webOS3 Attempting to wake via CEC");
-  	      cec.send("on 0");
+  	      cec.send("e0:04");
 	      return callback(null, true);
       } else {
 		  // Attempt to wake via WOL.      
